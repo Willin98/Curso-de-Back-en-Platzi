a1208badf03fb7ad32b1f34156648ad8fb178fd7
Willin98 <67207973+Willin98@users.noreply.github.com>
2023-02-14 14:22:48 -0500

    Manejando middlewares y manejo de los datos

--- curso-backend-Node.js/routes/products.router.js
+++ curso-backend-Node.js/routes/products.router.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ProductsService = require('./../services/product.service');
 const service = new ProductsService();
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -12,36 +14,38 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter')
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatorHandler(getProductSchema, 'params'), async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', validatorHandler(createProductSchema, 'body'), async (req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  res.status(201).json(newProduct);
+});
+
+router.patch('/:id', validatorHandler(getProductSchema, 'params'), validatorHandler(updateProductSchema, 'body'), async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.delete(id);
   res.json(product);
-});
-
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
-});
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id,
-  });
 });
 
 module.exports = router;
--- curso-backend-Node.js/services/product.service.js
+++ curso-backend-Node.js/services/product.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -12,27 +13,58 @@ class ProductsService {
     for (let index = 0; index < limit; index++) {
       this.products.push({
         id: faker.datatype.uuid(),
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+        isblock: faker.datatype.boolean(),
     })
     }
   }
 
-  create() {
-
+  async create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.products.push(newProduct);
+    return newProduct;
   }
 
-  find() {
+  async find() {
     return this.products;
   }
 
-  findOne(id) {
-    return this.products.find(item => item.id === id);
+  async findOne(id) {
+    const product = this.products.find(item => item.id === id);
+    if(!product) {
+      throw boom.notFound('product not found');
+    }
+    if(product.isblock) {
+      throw boom.conflict('product is block');
+    }
+    return product;
   }
 
-  delete() {
+  async update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1) {
+      throw boom.notFound('product not found');
+    }
+    const product =   this.products[index]
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
 
+  async delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1) {
+      throw boom.notFound('product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
   }
 }
 
